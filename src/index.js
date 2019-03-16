5f0aaab4cdb852902f81f331cec26cb0b1521592
Hanjun Kim <hallazzang@gmail.com>
2019-03-16 04:41:37 +0000

    test with basic project structure

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of blah blah',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
